e8a8a526ada114542adc6c1fb7c01afaca1b86c0
HenryRiddall <henryriddall@gmail.com>
2021-06-29 15:10:52 +0100

    Updates

--- index.ts
+++ index.ts
@@ -1,32 +1,49 @@
-const readline = require('readline').createInterface({
+import * as readline1 from 'readline';
+
+// enable and disable each rule 1 for enabled 0 for disabled default is enabled
+// e.g. all disabled is 0 0 0 0 0
+let args: string[] = process.argv.slice(2);
+
+// names for each rule
+const fizzFlag: boolean = args[0] != "0"
+const bangFlag: boolean = args[1] != "0"
+const bongFlag: boolean = args[2] != "0"
+const fezzFlag: boolean = args[3] != "0"
+const revFlag: boolean = args[4] != "0"
+
+
+const readline = readline1.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-readline.question("How many numbers do you want? ", (nums: number) => {
-    nums++
-    for (let i = 1; i < nums; i++) {
+
+readline.question("How many numbers do you want? ", (maxValue) => {
+    // parseInt(qq: string) -> number
+    let nums: number = parseInt(maxValue)
+    for (let i = 1; i <= nums; i++) {
         let words: string[] = [];
-        if (i % 11 == 0){
-            if (i % 13 == 0) {
+        if (i % 11 == 0 && bongFlag){
+            if (i % 13 == 0 && fezzFlag) {
                 words.push("Fezz")
             }
             words.push("Bong")
         }
         else {
             let printNum: boolean = true;
-            if (i % 3 == 0) {
+            // continue
+            if (i % 3 == 0 && fizzFlag) {
                 words.push("Fizz")
                 printNum = false
             }
-            if (i % 13 == 0) {
+            if (i % 13 == 0 && fezzFlag) {
                 words.push("Fezz")
                 printNum = false
             }
-            if (i % 5 == 0) {
+            if (i % 5 == 0 && fizzFlag) {
                 words.push("Buzz")
                 printNum = false
             }
-            if (i % 7 == 0) {
+            if (i % 7 == 0 && bangFlag) {
                 words.push("Bang")
                 printNum = false
             }
@@ -34,7 +51,7 @@ readline.question("How many numbers do you want? ", (nums: number) => {
                 words.push(i.toString())
             }
         }
-        if (i % 17 == 0){
+        if (i % 17 == 0 && revFlag){
             words.reverse()
         }
         words.forEach(function (word){
@@ -42,5 +59,5 @@ readline.question("How many numbers do you want? ", (nums: number) => {
         });
         process.stdout.write("\n")
     }
-    return
+    readline.close()
 });
